b830ac1329e75ef17bc01ae0d69f26a58db68f77
Konstantin-Gol <ctw21.konstantin.goliskins@corketb.ie>
2025-09-09 14:19:05 +0100

    Add files via upload

---
+++ 5th year basics/StarterProgramLesson.py
@@ -0,0 +1,19 @@
+# starter program lesson 1
+
+name = input("What is your name?: ")
+print("We want to know if you like programming!")
+answer = input("Do you like programming " + name + "?: ")
+print("Great! You said " + answer + "!")
+print("Let's learn some Python today")
+#Q1: 2 and they're used for the hold and redistrobution of value
+#Q2: input is used to add value to a variable through string
+#Q3: the + is used to add the variables to the code
+#Q4: it will cause an error and not work
+
+name = "Billy"
+print("We want to know if you like programming!")
+print()
+print("Do you like programming " + name + "?")
+answer = input("Answer:")
+print("Great! You said " + answer + "!")
+print("Let's learn some Python today")
---
+++ 5th year basics/questionnare 1.py
@@ -0,0 +1,5 @@
+#questionnare1
+name = input("What is your name?: ")
+print(name, "that's a nice name")
+age = input("By the way how old are you?: ")
+breakFast = input("Oh before I forget what did u have for breakfast?: ")
