e92f9ca9c9d4e77c33fa45a62cfad78f5351b9f1
Konstantin-Gol <ctw21.konstantin.goliskins@corketb.ie>
2025-09-15 13:42:23 +0100

    Add files via upload

---
+++ 5th year basics/cities question.py
@@ -0,0 +1,9 @@
+def cities():
+    print("how many cities are there in England?")
+    answer = int(input())
+    if answer == 51:
+        print("correct")
+    else:
+        print("Incorrect. the answer is 51")
+        
+cities()
