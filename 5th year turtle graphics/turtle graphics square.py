ac5ca9a516a93e83b678ba7841bb92187b9943a5
Konstantin-Gol <ctw21.konstantin.goliskins@corketb.ie>
2025-09-16 14:20:01 +0100

    Add files via upload

---
+++ 5th year turtle graphics/turtle graphics square.py
@@ -0,0 +1,17 @@
+from turtle import *
+
+
+def square ():
+    fillcolor("blue")
+    begin_fill()
+    pencolor("green")
+    forward (150) 
+    right (90)
+    forward (150) 
+    right (90) 
+    forward (150) 
+    right (90) 
+    forward(150)  
+    end_fill()
+
+square()
---
+++ 5th year turtle graphics/turtle graphics triangle.py
@@ -0,0 +1,16 @@
+from turtle import *
+
+
+def triangle ():
+    fillcolor("yellow")
+    begin_fill()
+    pencolor("blue")
+    right(60)
+    forward (120)
+    right(120)
+    forward (120)
+    right(120)
+    forward (120)
+    end_fill()
+
+triangle()
