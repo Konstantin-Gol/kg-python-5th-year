eef4f69b5bc135aad69867646d32249690d36678
Konstantin-Gol <ctw21.konstantin.goliskins@corketb.ie>
2025-09-22 23:43:59 +0100

    Add files via upload

---
+++ 5th year turtle graphics/resizeable triangle and square.py
@@ -0,0 +1,15 @@
+from turtle import *
+
+def square(square_sides_length):
+    for counter in range(4):
+        forward(square_sides_length)
+        right(90)
+
+def triangle(triangle_sides_length):
+    for counter in range(3):
+        fd(triangle_sides_length)
+        left(120)
+square_sides_length = int(input("what size doe you want your square to be?"))
+triangle_sides_length = int(input("what size doe you want your triangle to be?"))
+square(triangle_sides_length)
+triangle(triangle_sides_length)
