d6d1173ab51ee76dcafda547ec44d527035bc030
Konstantin-Gol <ctw21.konstantin.goliskins@corketb.ie>
2025-09-22 13:37:58 +0100

    Update envelope.py

--- 5th year turtle graphics/envelope.py
+++ 5th year turtle graphics/envelope.py
@@ -1,12 +1,13 @@
 from turtle import *
 
 def envelope():
-    for square in range (4):
+    for square in range (2):
         right(90)
         fd(100)
-    right(150)
+        right(90)
+        fd(140)
+    right(140)
     for triangle in range (2):
-        fd(50)
-        left(180)
-
+        fd(93)
+        right(81)
 envelope()
