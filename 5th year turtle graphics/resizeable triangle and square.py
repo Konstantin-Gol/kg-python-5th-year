2b028b4ad28e97afbafa243800cf5a37d52dacd8
Konstantin-Gol <ctw21.konstantin.goliskins@corketb.ie>
2025-09-23 14:15:08 +0100

    Update resizeable triangle and square.py

--- 5th year turtle graphics/resizeable triangle and square.py
+++ 5th year turtle graphics/resizeable triangle and square.py
@@ -6,10 +6,10 @@ def square(square_sides_length):
         right(90)
 
 def triangle(triangle_sides_length):
-    for counter in range(3):
+    for tringle in range(3):
         fd(triangle_sides_length)
         left(120)
-square_sides_length = int(input("what size doe you want your square to be?"))
-triangle_sides_length = int(input("what size doe you want your triangle to be?"))
-square(triangle_sides_length)
+square_sides_length = int(input("what size doe you want your square to be? "))
+triangle_sides_length = int(input("what size doe you want your triangle to be? "))
+square(square_sides_length)
 triangle(triangle_sides_length)
