079cbab197b448bf186bdbbd0ce46e254f5a22e3
Konstantin-Gol <ctw21.konstantin.goliskins@corketb.ie>
2025-10-06 14:17:46 +0100

    Add files via upload

---
+++ 5th year basics/string help.py
@@ -0,0 +1,17 @@
+#string help
+subject = "ComputerScience"
+ 
+# Extract "Computer" (0 to 8)
+print(subject[0:8])
+ 
+# Extract "Science" (8 to end)
+print(subject[8:])
+ 
+# Extract first 3 characters
+print(subject[:3])
+ 
+# Extract last 3 characters
+print(subject[-3:])
+ 
+# Reverse the string
+print(subject[::-1])
---
+++ 5th year basics/string intro.py
@@ -0,0 +1,7 @@
+#string intro
+word = "konstantin Goliskins"
+print(word[0])
+print(word[4])
+print(word[11])
+print(word[0])
+print(len(word)) #how man letter :D
