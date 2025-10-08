2e4d1187720dc3a7cefd0a3dcd3a0a4cc14cf519
Konstantin-Gol <ctw21.konstantin.goliskins@corketb.ie>
2025-10-08 23:33:58 +0100

    Add files via upload

---
+++ 5th year basics/the 5 methods.py
@@ -0,0 +1,12 @@
+#the 5 methods
+text = 'I luv pyth0n'
+EG1 = text.replace("luv", "hate") #replaces text
+EG2 = text.replace("python", "java")
+EG3 = text.islower() # makes text lowercase
+EG4 = input('enter text to be lowercased: ').islower()
+EG5 = text.isupper() #makes text uppercase
+EG6 = input('enter text to be uppercased: ').islower()
+EG7 = text.isalpha()
+EG8 = input('im going to check if this is only letters: ').isalpha()
+EG9 = text.isdigit()
+EG10 = input('im going to check if this is only numbers: ').isdigit()
