621d331ede0bdb10359fc643b56c8faf87af49ad
Konstantin-Gol <ctw21.konstantin.goliskins@corketb.ie>
2025-10-09 09:40:20 +0100

    Update the 5 methods.py

--- 5th year basics/the 5 methods.py
+++ 5th year basics/the 5 methods.py
@@ -2,11 +2,11 @@
 text = 'I luv pyth0n'
 EG1 = text.replace("luv", "hate") #replaces text
 EG2 = text.replace("python", "java")
-EG3 = text.islower() # makes text lowercase
+EG3 = text.islower() # checks if text is lowercase
 EG4 = input('enter text to be lowercased: ').islower()
-EG5 = text.isupper() #makes text uppercase
+EG5 = text.isupper() #checks if text is uppercase
 EG6 = input('enter text to be uppercased: ').islower()
-EG7 = text.isalpha()
+EG7 = text.isalpha() #checks if text is only letters
 EG8 = input('im going to check if this is only letters: ').isalpha()
-EG9 = text.isdigit()
+EG9 = text.isdigit() #checks if text is only numbers
 EG10 = input('im going to check if this is only numbers: ').isdigit()
