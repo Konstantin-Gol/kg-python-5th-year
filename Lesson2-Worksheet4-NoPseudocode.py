7f44fb0d8b3365af03b3057a11e8fd509c29799f
Konstantin-Gol <ctw21.konstantin.goliskins@corketb.ie>
2025-10-16 10:11:05 +0100

    Update Lesson2-Worksheet4-NoPseudocode.py

--- Lesson2-Worksheet4-NoPseudocode.py
+++ Lesson2-Worksheet4-NoPseudocode.py
@@ -6,15 +6,17 @@
 # Between 5-15 characters
 # Only letters and numbers
 # Must start with a letter
-username = input("please enter a username: ")
 
-def username_check():
-    username_pass = 0
-    if username >= 5:
-        username_pass = username_pass + 1
-    elif username <= 15:
-        username_pass = username_pass + 1
-first_letter = username[0]
-char1 = first_letter.isalpha()
-    if char1 is True:
-        username_pass += 1
+def username_check(username):
+    if not len(username) >= 5 or not len(username) <= 15:
+        return "Username must be 5-15 characters"
+    if not username[0].isalpha():
+        return "Username must start with a letter"
+    if not username.isalnum():
+        return "Username can only contain letters and numbers"
+    else:
+        return "valid username"
+    
+username = input("please enter a username: ")
+username_check(username)
+print(username_check(username))
