d87387574f1c9b0e89c2045cd0db9e61b1c14a63
Konstantin-Gol <ctw21.konstantin.goliskins@corketb.ie>
2025-10-21 14:11:56 +0100

    Add files via upload

---
+++ section 1.2.py
@@ -0,0 +1,6 @@
+#section 1.2
+print('I will find how old you are') #to ease the user in
+current_year = 2025 #to be minused
+birth_year = int(input("enter your birth year: "))#for the age
+birth_year = current_year - birth_year #badabing bading it's doing a calculation
+print("As of 2025 you are", birth_year) #tadaaa it's done
---
+++ section1.1.py
@@ -0,0 +1,7 @@
+#section 1.1
+student_name = "konstantin"
+age = 17
+height = 1.90
+print("name:",student_name,"=", type(student_name))#shows thats it's type is a string
+print("age:",age,"=", type(age))#shows thats it's type is an interger
+print("height:",height,"=", type(height))#shows thats it's type is a float
