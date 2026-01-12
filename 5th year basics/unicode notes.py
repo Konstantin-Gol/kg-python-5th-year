f5bc944cef95ff942f3d90842bd4c02f21cbb546
Konstantin-Gol <ctw21.konstantin.goliskins@corketb.ie>
2026-01-12 14:13:35 +0000

    Add files via upload

---
+++ 5th year basics/unicode notes.py
@@ -0,0 +1,13 @@
+print("String Value: u")
+print("Convert to unicode:", ord("u"))
+
+letter = input("Initials of first name: ")
+print("the letter", letter, "converts to unicode value", ord(letter))
+
+letter = input("Initials of surname: ")
+print("the letter", letter, "converts to unicode value", ord(letter))
+
+print("Convert 75 to character:", chr(75))
+
+number = int(input("Number: "))
+print("the number", number, "converts to character", chr(number))
